4b816371a273fd6e37254cd3549b5bb1e1751836
Muhammad Rehan Bukhari <mrehanbukhari2001@gmail.com>
2026-03-04 03:10:58 -0800

    Update main.c

--- 02-timers/main.c
+++ 02-timers/main.c
@@ -1,49 +1,19 @@
-/*
- * Copyright (C) 2022 HAW Hamburg
- *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
- */
-
 #include <stdio.h>
-
 #include "ztimer.h"
-/* needed to manipulate the LEDs */
 #include "board.h"
-
-void message_callback(void *argument)
-{
-    char *message = (char *)argument;
-    puts(message);
-}
-
-/* [TASK 3: insert your callback function here] */
 
 int main(void)
 {
-    puts("This is a timers example");
+    puts("Starting my custom LED blink program");
 
-    /* we can configure an event to occur in the future by setting a timer */
-    ztimer_t timeout;                     /* create a new timer */
-    timeout.callback = message_callback;  /* set the function to execute */
-    timeout.arg = "Timeout!";             /* set the argument that the function will receive */
-    ztimer_set(ZTIMER_SEC, &timeout, 2);  /* set the timer to trigger in 2 seconds */
-
-    /* [TASK 3: insert your timer here] */
-
-    /* in parallel, we can perform other tasks on this thread */
-    /* get the current timer count */
-    ztimer_now_t start = ztimer_now(ZTIMER_MSEC);
-
-    /* blink an LED for 10 seconds */
-    while ((ztimer_now(ZTIMER_MSEC) - start) <= 10000) {
-        /* this blinks the LED twice a second */
-        LED0_TOGGLE;
-        ztimer_sleep(ZTIMER_MSEC, 500);
+    /* blink LED 10 times with 1 second delay */
+    for (int i = 0; i < 10; i++) {
+        LED0_TOGGLE;                      // turn LED ON/OFF
+        puts("LED toggled");
+        ztimer_sleep(ZTIMER_SEC, 1);      // wait 1 second
     }
 
-    puts("Done!");
+    puts("Program finished");
 
     return 0;
 }
